9f597687358d582a06c808b9e0eb3433f8c04f67
Pablo Antuña <pablo.antuna99@gmail.com>
2023-02-08 14:25:54 -0300

    comentado temporalmente para poder buildear

--- src/FiniteAutomata.hs
+++ src/FiniteAutomata.hs
@@ -17,11 +17,13 @@ reverseAefd = undefined
 sideAefd :: AEFDG -> AEFDG
 sideAefd (D s sts f stsa sti b) = D s sts f stsa sti (not b)
 
-aefndToAEFD :: AEFND a -> AEFD [a]
-aefndToAEFD (ND a sts (RelT r) stsa sti b) = 
-  let a' = 
-      sts' =
-      f =
-      stsa' = 
-      sti' = St ([runSt sti] ++ [runSt s | s <- sts, elem (sti, "", s) r]) -- el estado inicial nuevo es el estado inicial + los estados a los cuales desde el inicial puedo hacer una lambda/empty transition. Falta ver tema repetido tal vez?
-   in D a' sts' (FunT f) stsa' sti' b
+-- aefndToAEFD :: AEFND a -> AEFD [a]
+-- aefndToAEFD (ND a sts (RelT r) stsa sti b) = 
+--   let a' = 
+--       sts' =
+--       f =
+--       stsa' = 
+--       sti' = St ([runSt sti] ++ [runSt s | s <- sts, elem (sti, "", s) r]) -- el estado inicial nuevo es el estado inicial + los estados a los cuales desde el inicial puedo hacer una lambda/empty transition. Falta ver tema repetido tal vez?
+--    in D a' sts' (FunT f) stsa' sti' b
+
+aefndToAEFD = undefined
