790ed319c08e36902143593d7e933ed145c447d5
Pablo Antuña <pablo.antuna99@gmail.com>
2023-02-12 21:08:11 -0300

    por si acaso mas deriving

--- src/Common.hs
+++ src/Common.hs
@@ -8,12 +8,13 @@ module Common where
 
   -- GramTerm es una gramatica regular vista de forma mas general (si es izq o der esta determinado en otra estructura)
   newtype GramTerm = Gram [Rule]
-    deriving Show
+    deriving (Eq, Ord, Show)
 
   -- una regla de produccion de una gramatica esta dada por
   -- un no terminal que seria A en A -> generacion1 | generacion2 y una lista que seria [generacion1, generacion2]
+  -- en caso de tener varias reglas A -> generacion1 A -> generacion2 seran 2 reglas distintas por mas que sean del mismo no terminal
   data Rule = Rule NT [RigthSide]
-    deriving Show
+    deriving (Eq, Ord, Show)
 
   -- no terminal
   data NT = NT String | Initial
@@ -24,7 +25,7 @@ module Common where
    deriving (Eq, Ord, Show)
 
   data RigthSide = RT T | RTNT T NT | RL
-    deriving Show
+    deriving (Eq, Ord, Show)
 
   -- la gramatica regular puede ser izquierda o derecha
   -- lo bueno de usar haskell es que tenemos
@@ -32,30 +33,6 @@ module Common where
   -- asi que Left va a ser una gramatica izq y Right una gramatica der
   -- (would be very funny swap it pero se que despues voy a estar enojado conmigo mismo)
   type Gram = Either GramTerm GramTerm
-
-  -- los terminales (esto inicialmente lo habia pensado como simplemente hacerlo string y no un newtype pero quiero probar asi porque se ve mas indicativo de que es)
-  -- newtype T = T { runT :: String }
-  --   deriving (Eq, Ord, Show)
-  
-  -- los no terminales (esto inicialmente lo habia pensado como simplemente hacerlo string y no un newtype pero quiero probar asi porque se ve mas indicativo de que es)
-  -- newtype NT = NT { runNT :: String }
-  --   deriving (Eq, Ord, Show)
-
-  -- regla de produccion gram izquierda
-  -- data RulesL = RLT NT T
-  --             | RLNT NT NT T
-  --             | RLL NT -- lambda, el NT este genera la cadena vacia
-
-  -- regla de produccion gram der
-  -- data RulesR = RRT NT T
-  --             | RRNT NT T NT
-  --             | RRL NT -- lambda, el NT este genera la cadena vacia
-
-  -- data LGrm = LG [T] [NT] [RulesL]
-
-  -- data RGrm = RG [T] [NT] [RulesR]
-
-  -- type Grm = Either LGrm RGrm
 
   -- por lo que estuve leyendo se que me voy a tener que meter con pasar
   -- las gramaticas a automatas asi que seguro termino necesitando guardarlas de esa forma
@@ -65,7 +42,7 @@ module Common where
     deriving (Eq, Ord, Show)
 
   -- simbolos automatas deterministas
-  newtype SimbD = SimbD {runSimbD :: NonEmpty Char} -- no puedo tener empty transition en un dfa (no puedo pasar al siguiente estado usando al cadena vacia)
+  newtype SimbD = SimbD {runSimbD :: NonEmpty Char} -- no puedo tener empty transition en un dfa (no puedo pasar al siguiente estado usando la cadena vacia)
     deriving (Eq, Ord, Show)
 
   newtype St a = St {runSt :: a}
@@ -81,8 +58,10 @@ module Common where
 
   -- alfabeto - lista de estados - funcion de transicion - estados de aceptacion - estado inicial
   data AEFD a = D [SimbD] [St a] (FunT a) [St a] (St a) Bool  -- el bool es para indicar si era izq o der. false -> izq, true -> der
+    deriving Show
 
   data AEFND a = ND [SimbND] [St a] (RelT a) [St a] (St a) Bool  -- el bool es para indicar si era izq o der. false -> izq, true -> der
+    deriving Show
 
   type AEFDG = AEFD Int
 
