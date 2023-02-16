951b2f29c66ee369909b53bf5dc4ecbd59ec292e
Pablo Antuña <pablo.antuna99@gmail.com>
2023-02-16 17:36:45 -0300

    agregados comentarios con algunos detalles

--- src/Common.hs
+++ src/Common.hs
@@ -24,6 +24,7 @@ module Common where
   newtype T = T {runT :: String}
    deriving (Eq, Ord, Show)
 
+  -- en el lado derecho de una regla de produccion tenemos un terminal, un terminal y un no terminal o lambda
   data RigthSide = RT T | RTNT T NT | RL
     deriving (Eq, Ord, Show)
 
@@ -45,6 +46,7 @@ module Common where
   newtype SimbD = SimbD {runSimbD :: NonEmpty Char} -- no puedo tener empty transition en un dfa (no puedo pasar al siguiente estado usando la cadena vacia)
     deriving (Eq, Ord, Show)
 
+  -- estado
   newtype St a = St {runSt :: a}
     deriving (Eq, Ord, Show)
 
@@ -60,9 +62,15 @@ module Common where
   data AEFD a = D [SimbD] [St a] (FunT a) [St a] (St a) Bool  -- el bool es para indicar si era izq o der. false -> izq, true -> der
     deriving Show
 
+  -- alfabeto - lista de estados - funcion de transicion - estados de aceptacion - estado inicial
   data AEFND a = ND [SimbND] [St a] (RelT a) [St a] (St a) Bool  -- el bool es para indicar si era izq o der. false -> izq, true -> der
     deriving Show
 
+  -- al automata de estado finito determinista que usamos para representar la gramatica
+  -- lo vamos a definir con un estandar donde el tipo de dato que manejan sus estados son Int por convencion y facilidad a la hora de trabajar
+  -- esto nos genera algunas limitaciones porque no tenemos por ejemplo String que nos permitiria mantener los nombres de los estados tal como 
+  -- se va llevando en el transcurso del programa lo cual nos serviria para mostrarle al usuario los mismo nombres que ellos ingresan (para las reglas de prod)
+  -- pero como no vamos a imprimir la gramatica original que ellos ingresan si no una equivalente no importa
   type AEFDG = AEFD Int
 
   -- entorno de gramaticas con sus nombres
@@ -70,15 +78,15 @@ module Common where
   -- type Env = M.Map Name AEFDG
 
   -- la parte de operaciones (union interseccion definicion etc)
-  data Op = OpDef Name OpGram
-          | OpIn String OpGram
-          | OpEqual OpGram OpGram
+  data Op = OpDef Name OpGram -- definicion de una gramatica (nombre = operacion entre gramaticas que ya existen o que se construyen como mas operaciones)
+          | OpIn String OpGram -- consulta si una palabra pertenece al lenguaje representado por una gramatica (nuevamente la gramatica por su nombre si ya existe o como la generacion de una gramatica a traves de operaciones)
+          | OpEqual OpGram OpGram -- consulta la igualdad de dos gramaticas (ya sea por nombre si ya existe o generada por operaciones)
 
-  data OpGram = OpGram Name
-              | OpUnion OpGram OpGram
-              | OpIntersec OpGram OpGram
-              | OpDiff OpGram OpGram
-              | OpConcat OpGram OpGram
-              | OpComplement OpGram
-              | OpReverse OpGram
-              | OpSide OpGram
+  data OpGram = OpGram Name -- el nombre de una gramatica, se la busca en el entorno
+              | OpUnion OpGram OpGram -- la union de dos gramaticas
+              | OpIntersec OpGram OpGram -- la interseccion de dos gramaticas
+              | OpDiff OpGram OpGram -- la resta de dos gramaticas
+              | OpConcat OpGram OpGram -- la concatenacion de dos gramaticas
+              | OpComplement OpGram -- el complemento de una gramatica
+              | OpReverse OpGram -- el reverso de una gramatica
+              | OpSide OpGram -- el cambio de lado (izq -> der, der -> izq) de una gramatica
